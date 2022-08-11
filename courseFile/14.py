8254a286eff217b9aecb0f52fcce90cd370288d4
ruthyarus12-3 <110621586+ruthyarus12-3@users.noreply.github.com>
2022-08-11 20:17:02 +0300

    Update 14.py

--- courseFile/14.py
+++ courseFile/14.py
@@ -2,7 +2,7 @@ import ast
 
 my_file = open("config.json")
 c = dict(ast.literal_eval((my_file.read())))
-if c["name"] == "ruth":
+if c["name"] == "ruth3":
     print("i love devops experts")
 
 with open("names.txt") as my_file:
